54b94c17cb18322fb25a94822e3e72cb5a80915e
danpaiva9 <danielpaiva9@gmail.com>
2021-07-22 19:13:20 -0300

    Third commit

--- script.js
+++ script.js
@@ -1,68 +1,87 @@
 // SCRIPT CADASTRO
-function cadastroCliente(){
-    let dadoNome = document.getElementById("campoNome");
-    let dadoEmail = document.getElementById("campoEmail");
-    let dadoTelefone = document.getElementById("campoTelefone");
-    let dadoRua = document.getElementById("campoRua");
-    let dadoCidade = document.getElementById("campoCidade");
-  
-    let dados = JSON.parse(localStorage.getItem("dadosCadastro"));
-  
-    if(dados == null){
-      localStorage.setItem("dadosCadastro", "[]");
-      dados = [];
-    }
-  
-    let auxRegistro = {
-      nome: dadoNome.value,
-      email: dadoEmail.value,
-      telefone: dadoTelefone,
-      rua: dadoRua,
-      cudade: dadoCidade
-    }
-    
-    dados.push(auxRegistro);
-  
-    localStorage.setItem("dadosCadastro", JSON.stringify(dados));
-    alert("Cadastro realizado com sucesso. Obrigado!");
-  
-    dadoNome.value = "";
-    dadoEmail.value = "";
-    dadoTelefone.value = "";
-    dadoRua.value = "";
-    dadoCidade.value = "";
+function cadastroCliente() {
+  let dadoNome = document.getElementById("campoNome");
+  let dadoEmail = document.getElementById("campoEmail");
+  let dadoTelefone = document.getElementById("campoTelefone");
+  let dadoRua = document.getElementById("campoRua");
+  let dadoCidade = document.getElementById("campoCidade");
+
+  let dados = JSON.parse(localStorage.getItem("dadosCadastro"));
+
+  if (dados == null) {
+    localStorage.setItem("dadosCadastro", "[]");
+    dados = [];
   }
 
-  // SCRIPT CADASTRO
-function cadastroProduto(e){
+  let auxRegistro = {
+    nome: dadoNome.value,
+    email: dadoEmail.value,
+    telefone: dadoTelefone.value,
+    rua: dadoRua.value,
+    cudade: dadoCidade.value
+  }
 
-    let dadoNomeProduto = document.getElementById("campoNamePorduct");
-    let dadoDescricao = document.getElementById("campoDescritionProduct");
-    let dadoID = document.getElementById("campoIdProduct");
-    let dadoPreco = document.getElementById("campoPriceProduct");
+  dados.push(auxRegistro);
 
-  
-    let dados = JSON.parse(localStorage.getItem("dadosCadastroProduto"));
-  
-    if(dados == null){
-      localStorage.setItem("dadosCadastroProduto", "[]");
-      dados = [];
-    }
-  
-    let auxRegistro = {
-      nomeProduto: dadoNomeProduto.value,
-      descricao: dadoDescricao.value,
-      ID: dadoID.value,
-      preco: dadoPreco.value
-    }
-    
-    dados.push(auxRegistro);
-  
-    localStorage.setItem("dadosCadastroProduto", JSON.stringify(dados));
-    alert("Cadastro realizado com sucesso. Obrigado!");
-  
-    dadoNomeProduto.value = "";
-    dadoDescricao.value = "";
-    dadoID.value = "";
-    dadoPreco.value = "";
+  localStorage.setItem("dadosCadastro", JSON.stringify(dados));
+  alert("Cadastro realizado com sucesso. Obrigado!");
+
+  dadoNome.value = "";
+  dadoEmail.value = "";
+  dadoTelefone.value = "";
+  dadoRua.value = "";
+  dadoCidade.value = "";
 }
+
+// SCRIPT CADASTRO
+function cadastroProduto() {
+
+  let dadoNomeProduto = document.getElementById("campoNamePorduct");
+  let dadoDescricao = document.getElementById("campoDescritionProduct");
+  let dadoID = document.getElementById("campoIdProduct");
+  let dadoPreco = document.getElementById("campoPriceProduct");
+
+
+  let dados = JSON.parse(localStorage.getItem("dadosCadastroProduto"));
+
+  if (dados == null) {
+    localStorage.setItem("dadosCadastroProduto", "[]");
+    dados = [];
+  }
+
+  let auxRegistro = {
+    nomeProduto: dadoNomeProduto.value,
+    descricao: dadoDescricao.value,
+    ID: dadoID.value,
+    preco: dadoPreco.value
+  }
+
+  dados.push(auxRegistro);
+
+  localStorage.setItem("dadosCadastroProduto", JSON.stringify(dados));
+  alert("Cadastro realizado com sucesso. Obrigado!");
+
+  dadoNomeProduto.value = "";
+  dadoDescricao.value = "";
+  dadoID.value = "";
+  dadoPreco.value = "";
+}
+
+function iniciaModal(modalID) {
+  const modal = document.getElementById(modalID);
+  if (modal) {
+
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+      if (e.target.id == modalID || e.target.id == 'fecharModal' || e.target.id == 'campoButtonCancel') {
+        modal.classList.remove('mostrar');
+      }
+    })
+  }
+}
+
+const usuario = document.querySelector('.buttonUsuario');
+usuario.addEventListener('click', () => iniciaModal('modalCadastro'));
+
+const produto = document.querySelector('.buttonProduto');
+produto.addEventListener('click', () => iniciaModal('modalCadastroProduto'));
